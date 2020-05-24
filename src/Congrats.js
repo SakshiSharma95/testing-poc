5e7f3d23a1ed147b09022d5da1dcc9c5966cbdc9
Sakshi Sharma <sakshi.sharma@tothenew.com>
2020-05-25 01:34:44 +0530

    Added congeates component and testUtils

---
+++ src/Congrats.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+/**
+ * Function react component for congratulatory messages
+ * @function
+ * @param {object} props : React props.
+ * @returns {JSX.Element} - Rendered component (or null if 'success' props is not received)
+ */
+
+export default (props) => {
+    if (props.success) {
+        return (
+            <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                Congratulations! You guessed the word!
+                </span>
+            </div>
+        )
+    } else {
+        return (
+            <div data-test="component-congrats"/>
+        );
+    }
+};
