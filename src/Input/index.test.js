10a1f5f2ec8c676bdb0effd9d9f92350243f7081
Sakshi Sharma <sakshi.sharma@tothenew.com>
2020-05-26 16:24:20 +0530

    storeFactory added and input component added

---
+++ src/Input/index.test.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { shallow } from 'enzyme';
+
+import { findByTestAttr, storeFactory } from "../../test/testUtils";
+import Input from './index';
+/**
+ * Factory function to create a ShallowWrapper for the Input component.
+ * @function setup
+ * @param {object} initialState - Initial state for this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (initialState = {}) => {
+    const store = storeFactory(initialState);
+    const wrapper = shallow(<Input store={store}/>).dive().dive();
+    return wrapper;
+};
+
+describe('render', () => {
+    describe('word has not been guessed', () => {
+        test('renders component without error', () => {
+
+        });
+        test('renders input box', () => {
+
+        });
+        test('renders submit button', () => {
+
+        })
+    });
+    describe('word has  been guessed', () => {
+        test('renders component without error', () => {
+
+        });
+        test('does not renders input box', () => {
+
+        });
+        test('does not renders submit button', () => {
+
+        })
+    })
+});
+
+describe('update state', () => {
+
+});
