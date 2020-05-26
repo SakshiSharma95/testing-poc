60c842ebb8d835a358e1f38f31c40e07c2d56cdd
Sakshi Sharma <sakshi.sharma@tothenew.com>
2020-05-26 18:35:07 +0530

    guess word action

---
+++ src/integration.test.js
@@ -0,0 +1,24 @@
+import { storeFactory } from "../test/testUtils";
+import { guessWord } from './actions';
+
+describe('guessed word action creator', () =>{
+    describe('no guessed words', () =>{
+        test('update state correctly for unsuccessful guess', () =>{
+
+        });
+        test('update state correctly for successful guess', () =>{
+
+        });
+    });
+});
+
+describe('some guessed word', () =>{
+    describe('no guessed wordcs', () =>{
+        test('update state correctly for unsuccessful guess', () =>{
+
+        });
+        test('update state correctly for successful guess', () =>{
+
+        });
+    });
+});
